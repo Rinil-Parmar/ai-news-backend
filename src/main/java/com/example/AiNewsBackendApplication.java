2d9b0a81c876aa6ad44d47982b7697e664337387
Rinil-Parmar <parmarrinila2002@gmail.com>
2025-11-08 14:22:22 -0500

    init backend api

---
+++ src/main/java/com/example/AiNewsBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNewsBackendApplication.class, args);
+		System.out.println("Hello its working");
+	}
+
+}
